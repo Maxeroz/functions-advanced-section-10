afc6a629ff85143d5056067ad1646fe4bae1368f
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-08-01 21:12:50 +0400

    Functions Accepting Callback Functions

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 'use strict';
+
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋🏻');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+/*
+///////////////////////////////////////////////////
 // How Passing Arguments Works: Value vs. Reference
 
 const flight = 'LH234';
@@ -35,8 +68,9 @@ checkIn(flight, jonas);
 
 console.log(jonas);
 
+///////////////////////////////////////////////////
 // Default Parameters
-/*
+
 const bookings = [];
 
 const createBooking = function (
