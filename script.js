4359ba68b8917b99b28ec6cef48526894d4850d5
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-08-02 22:18:11 +0400

    The call and apply Methods

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
 'use strict';
 
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+
+  // book: function() {}
+
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Maksim Ozerskii');
+lufthansa.book(635, 'Mike Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+/*
+///////////////////////////////////////////////////
 // Functions Returning Functions
 
 const greet = function (greeting) {
@@ -20,7 +74,7 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi!')('Mate!');
 
-/*
+
 ///////////////////////////////////////////////////
 // Functions Accepting Callback Functions
 
